3ebbfb1f2d55427295eb38943cee4a864b2611d5
mohammednabeel123 <abdulmuminnabeelmohammed@gmail.com>
2026-03-17 17:29:34 +0300

    excercies on basics c lang

--- Basics/main.c
+++ Basics/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+// This is a simple C program that demonstrates the use of the printf function to display messages and variables on the console.
+int main() {
+    //casting
+    int a = 5.0;
+    double b = 2;
+    double res;
+    res = a / b; // This will perform floating-point division and result in 2.5
+    printf("The result of a / b is: %.2lf\n", res); // This will print the result with 2 decimal places
 
-int main()
-{
-    int number;
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    int num1 = 10, num2 = 20;
 
-    printf("Your number is %d\n", number);
 
-    return 0;
+
+    double result = num1 / num2; // This will perform floating-point division and result in 0.5
+    printf("The result of num1 / num2 is: %.2lf\n", result); // This will print the result with 2 decimal places
+
+    putchar('A');
+    putchar('\n');
+
+    char c;
+    c = getchar(); // This will read a single character from the user input
+    printf("You entered: %c\n", c); // This will print the character entered
+
+    char name[] = "Nabeel";
+    int length = strlen(name); // This will calculate the length of the string "Nabeel"
+    printf("The length of the name is: %d\n", length); // This will
+
+
+return 0;
 }
--- Basics/time_conversion.c
+++ Basics/time_conversion.c
@@ -3,14 +3,18 @@
 
 int main()
 {
-    int sec;
+    while(1){
+          int sec, hours,minutes, remaining_sec;
     printf("Enter an integer representing seconds: ");
     scanf("%d",&sec);
 
-    
+    hours = sec / 3600;
 
+    minutes = (sec % 3600) / 60;
 
- 
+    remaining_sec = sec % 60;
 
+    printf("%d seconds is %d hours, %d minutes, and %d seconds.\n",sec,hours,minutes,remaining_sec);
     return 0;
+    }
 }
